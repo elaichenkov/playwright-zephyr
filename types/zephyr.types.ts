36aa03140b8ea4d9f836ae562c8f05093daa36ac
Yevhen Laichenkov <elaichenkov@gmail.com>
2022-06-03 00:08:48 +0300

    feat(service): add axios config to the service

--- types/zephyr.types.ts
+++ types/zephyr.types.ts
@@ -1,17 +1,19 @@
-export type ZephyrOptions = {
-  host: string,
-  user?: string,
-  password?: string,
-  authorizationToken?: string,
-  projectKey: string
+import type { AxiosRequestConfig } from 'axios';
+
+export interface ZephyrOptions extends AxiosRequestConfig {
+  host: string;
+  user?: string;
+  password?: string;
+  authorizationToken?: string;
+  projectKey: string;
 }
 
 export type ZephyrStatus = 'Pass' | 'Fail' | 'Blocked' | 'Not Executed' | 'In Progress';
 
 export type ZephyrTestResult = {
-  testCaseKey: string,
-  status: ZephyrStatus,
-  environment?: string,
-  executionTime?: string,
-  executionDate?: string
-}
+  testCaseKey: string;
+  status: ZephyrStatus;
+  environment?: string;
+  executionTime?: string;
+  executionDate?: string;
+};
