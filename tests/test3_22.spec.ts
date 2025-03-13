4da6c427be8e1ba830bb1d64111652694a5d4517
Michael Naghavipour <m.naghavipour@reply.de>
2025-03-13 20:52:13 +0100

    add new tests

---
+++ tests/test3_22.spec.ts
@@ -0,0 +1,141 @@
+import { test, expect } from '@playwright/test';
+
+test('[T4] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T5] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright1.dev/docs/intro')
+});
+
+test('[T6] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T7] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T8] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T9] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T10] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright1.dev/docs/intro')
+});
+
+test('[T11] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T12] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T13] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T14] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T15] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T16] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T17] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright1.dev/docs/intro')
+});
+
+test('[T18] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T19] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T20] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright1.dev/docs/intro')
+});
+
+test('[T21] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T22] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
+
+test('[T23] second test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await page.locator('.getStarted_Sjon', { hasText: 'Get started' }).click()
+  await expect(page).toHaveURL('https://playwright.dev/docs/intro')
+});
